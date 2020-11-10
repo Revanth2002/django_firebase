d75c5a276e691abdd07b2b4202eff79100f1d3d4
Revanth2002 <ndrevanth09@gmail.com>
2020-11-10 21:49:41 +0530

    main first publish

---
+++ firebase/firebaseproject/firebaseproject/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+import pyrebase
+from django.contrib.auth import logout
+
+firebaseConfig = {
+  'apiKey': "AIzaSyA9lWECICW_Tbj4lahpB0VuzmjlGaLnnXo",
+  'authDomain': "login-demo-148c2.firebaseapp.com",
+  'databaseURL': "https://login-demo-148c2.firebaseio.com",
+  'projectId': "login-demo-148c2",
+  'storageBucket': "login-demo-148c2.appspot.com",
+  'messagingSenderId': "900718925900",
+  'appId': "1:900718925900:web:d74a97a3eacbc3efa77ce9",
+  'measurementId': "G-CE0QD1888L"
+};
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+def signIn(request):
+    return render(request,"signIn.html")
+
+def postsign(request):
+    email = request.POST.get('email')
+    passw = request.POST.get('pass')
+    try:
+        user = auth.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "Invalid Credentials"
+        return render(request,'signIn.html',{'messg':message})
+    print(user['idToken'])
+    session_id = user['idToken']
+    request.session['uid']=str(session_id)   
+    return render(request,'welcome.html',{"e":email})
+
+def loggedout(request):
+    logout(request)
+    return redirect('/')
+
+
+
+
+
